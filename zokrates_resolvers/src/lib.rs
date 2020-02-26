ef3786a6fce208ac97a93918e07c0b24bc43c664
dark64 <darem966@gmail.com>
2020-02-26 19:05:05 +0100

    refactor module resolvers

---
+++ zokrates_resolvers/src/lib.rs
@@ -0,0 +1,38 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+pub mod resolvers;
+
+use regex::Regex;
+use resolvers::*;
+use std::path::PathBuf;
+use zokrates_core::compile::{ResolvedModule, Resolver};
+use zokrates_core::imports::Error;
+
+lazy_static! {
+    static ref URL_REGEX: Regex = Regex::new(r#"https?://"#).unwrap();
+}
+
+pub struct SmartResolver {}
+impl SmartResolver {
+    pub fn new() -> Self {
+        SmartResolver {}
+    }
+}
+
+impl Resolver<Error> for SmartResolver {
+    fn resolve(
+        &self,
+        current_location: PathBuf,
+        import_location: PathBuf,
+    ) -> Result<ResolvedModule, Error> {
+        if URL_REGEX.is_match(import_location.to_str().unwrap()) {
+            url::UrlResolver::new().resolve(current_location, import_location)
+        } else {
+            fs::FileSystemResolver::new()
+                .resolve(current_location, import_location)
+                .map_err(|e| e.into())
+        }
+    }
+}
--- zokrates_resolvers/src/resolvers/url.rs
+++ zokrates_resolvers/src/resolvers/url.rs
@@ -1,40 +1,37 @@
-#[macro_use]
-extern crate lazy_static;
-extern crate regex;
-
-use regex::Regex;
 use reqwest::blocking::Response;
 use reqwest::StatusCode;
 use std::path::PathBuf;
 
+use zokrates_core::compile::{ResolvedModule, Resolver};
 use zokrates_core::imports::Error as ImportError;
 
-lazy_static! {
-    static ref PROTOCOL_REGEX: Regex = Regex::new(r#"https?://"#).unwrap();
+pub struct UrlResolver {}
+
+impl UrlResolver {
+    pub fn new() -> Self {
+        UrlResolver {}
+    }
 }
 
-pub fn resolve(
-    current_location: PathBuf,
-    import_location: PathBuf,
-) -> Result<(String, PathBuf), ImportError> {
-    let url = import_location.to_str().unwrap();
-    if PROTOCOL_REGEX.is_match(url.clone()) {
-        let response: Response = reqwest::blocking::get(url).unwrap();
+impl Resolver<ImportError> for UrlResolver {
+    fn resolve(
+        &self,
+        _: PathBuf,
+        import_location: PathBuf,
+    ) -> Result<ResolvedModule, ImportError> {
+        let url = import_location.to_str().unwrap();
+        let response: Response = reqwest::blocking::get(url)
+            .map_err(|_| ImportError::new(format!("{}", "Could not access remote url")))?;
+
         match response.status() {
-            StatusCode::OK => Ok((response.text().unwrap(), current_location.to_owned())),
+            StatusCode::OK => Ok((response.text().unwrap(), import_location.to_owned())),
             _ => Err(ImportError::new(format!(
                 "Unable to resolve module ({}): {}",
                 url.clone(),
                 response.text().unwrap()
             ))),
         }
-    } else {
-        Err(ImportError::new(format!("{}", "Invalid URL; http(s) protocol expected")))
     }
-}
-
-pub fn is_external_import(source: &str) -> bool {
-    PROTOCOL_REGEX.is_match(source)
 }
 
 #[cfg(test)]
@@ -43,8 +40,9 @@ mod tests {
 
     #[test]
     pub fn resolve_ok() {
-        let res = resolve(
-            PathBuf::new(),
+        let url_resolver = UrlResolver::new();
+        let res = url_resolver.resolve(
+            PathBuf::from("main"),
             PathBuf::from(
                 "https://raw.githubusercontent.com/Zokrates/ZoKrates/master/zokrates_cli/examples/add.zok",
             )
@@ -54,8 +52,9 @@ mod tests {
 
     #[test]
     pub fn resolve_err() {
-        let res = resolve(
-            PathBuf::new(),
+        let url_resolver = UrlResolver::new();
+        let res = url_resolver.resolve(
+            PathBuf::from("main"),
             PathBuf::from(
                 "https://raw.githubusercontent.com/Zokrates/ZoKrates/master/zokrates_cli/examples/unknown.zok",
             )
